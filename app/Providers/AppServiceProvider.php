ec72e8d6184ee16dd34285b7fc9fd66d46f79c93
Manav Hirey <manavhirey@gmail.com>
2021-04-17 23:00:58 +0530

    for css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,18 @@ use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+    }
+
+    /**
      * Register any application services.
      *
      * @return void
@@ -15,18 +27,5 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Schema::defaultStringLength(191);
-        if($this->app->environment('production')) {
-            \URL::forceScheme('https');
-                    }
     }
 }
